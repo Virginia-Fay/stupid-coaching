659f330f26463dc377898beef7ad27ecfbdd9a33
virginie lty <virginielty@gmail.com>
2021-12-09 19:05:32 +0100

    user / coach display

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,20 +1,17 @@
 class QuestionsController < ApplicationController
   def ask
-    @answer = params[:ask]
+
   end
 
   def answer
-    @answer = ""
+    @question = params[:question]
     # je récupère l'input de la view en appelant le params
-    if params[:ask].end_with?("?")
+    if params[:question].end_with?("?")
       @answer = "Silly question, get dressed and go to work!"
-      return @answer
-    elsif params[:ask] == "I am going to work"
+    elsif params[:question] == "I am going to work"
       @answer = "Great"
-      return @answer
     else
       @answer = "I don't care, get dressed and go to work!"
-      return @answer
     end
   end
 end
